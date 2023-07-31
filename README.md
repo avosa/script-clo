01f4347219267550f0069720e9a409f0b45536bb
Webster <websterb17@gmail.com>
2023-07-31 07:53:04 -0400

    Add support to install Ruby under the hood if platform doesn't that that installed yet

--- README.md
+++ README.md
@@ -3,7 +3,6 @@
 This script automates the installation process of Clojure and Leiningen, the popular tools for Clojure development. It provides support for macOS (both Intel and M1 chips), Linux (Debian-based and Fedora-based), and Windows (both 32-bit and 64-bit) platforms. The script detects the current environment and installs the appropriate versions of Clojure and Leiningen accordingly.
 
 ## Usage as a Shell Script
-To run the script as a shell script, make sure you have Ruby installed on your system, and then follow these steps:
 
 1. Open a terminal and navigate to the directory containing `clo.rb`.
 
@@ -24,7 +23,7 @@ chmod +x clo.rb
 ruby clo.rb
 ```
 
-The script will automatically detect your system architecture and operating system and install Clojure and Leiningen accordingly.
+The script will automatically detect your system architecture and operating system and install Ruby, Clojure and Leiningen accordingly.
 
 ## Note
 - The script may prompt for administrative privileges on macOS and Linux during package installation.
--- clo.rb
+++ clo.rb
@@ -1,153 +1,179 @@
 #!/usr/bin/env ruby
 
 def execute_command(command)
-    puts "Executing: #{command}"
-    output = `#{command}`
-    status = $?.exitstatus
-    [output, status]
+  puts "Executing: #{command}"
+  output = `#{command}`
+  status = $?.exitstatus
+  [output, status]
+end
+
+def install_ruby
+  case RbConfig::CONFIG['host_os']
+  when /darwin|linux-gnu/
+    return if system('command -v ruby >/dev/null 2>&1')
+
+    puts "Installing Ruby using RVM..."
+    execute_command('curl -sSL https://get.rvm.io | bash -s stable --ruby')
+    execute_command('source /usr/local/rvm/scripts/rvm')
+  when /mswin|mingw|cygwin/
+    return if system('command -v ruby >/dev/null 2>&1')
+
+    puts "Installing Ruby using Chocolatey..."
+    execute_command('choco install ruby')
+  else
+    puts "Unsupported OS: #{RbConfig::CONFIG['host_os']}. Cannot install Ruby."
+    exit(1)
   end
-  
-  def install_homebrew
-    return if system('command -v brew >/dev/null 2>&1')
-  
-    puts "Installing Homebrew..."
-    execute_command('/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"')
-  end
-  
-  def install_clojure
-    case RbConfig::CONFIG['host_os']
-    when /darwin/
-      case RbConfig::CONFIG['host_cpu']
-      when /x86_64/
-        install_homebrew
-        unless system('command -v clojure >/dev/null 2>&1')
-          puts "Installing Clojure..."
-          output, status = execute_command('brew install clojure')
-          puts output unless status.zero?
-        else
-          puts "Clojure is already installed."
-        end
-      when /arm64/
-        install_homebrew
-        unless system('command -v clojure >/dev/null 2>&1')
-          puts "Installing Clojure..."
-          output, status = execute_command('brew install clojure')
-          puts output unless status.zero?
-        else
-          puts "Clojure is already installed."
-        end
-      else
-        puts "Unsupported architecture: #{RbConfig::CONFIG['host_cpu']}. Cannot install Clojure."
-        exit(1)
-      end
-    when /linux-gnu/
-      if system('command -v apt-get >/dev/null 2>&1')
-        unless system('command -v clojure >/dev/null 2>&1')
-          puts "Updating package list..."
-          execute_command('sudo apt-get update')
-  
-          puts "Installing Clojure..."
-          output, status = execute_command('sudo apt-get install -y clojure')
-          puts output unless status.zero?
-        else
-          puts "Clojure is already installed."
-        end
-      elsif system('command -v dnf >/dev/null 2>&1')
-        unless system('command -v clojure >/dev/null 2>&1')
-          puts "Installing Clojure..."
-          output, status = execute_command('sudo dnf install -y clojure')
-          puts output unless status.zero?
-        else
-          puts "Clojure is already installed."
-        end
-      else
-        puts "Unsupported Linux distribution. Cannot install Clojure."
-        exit(1)
-      end
-    when /mswin|mingw|cygwin/
+end
+
+def install_homebrew
+  return if system('command -v brew >/dev/null 2>&1')
+
+  puts "Installing Homebrew..."
+  execute_command('/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"')
+end
+
+def install_clojure
+  case RbConfig::CONFIG['host_os']
+  when /darwin/
+    case RbConfig::CONFIG['host_cpu']
+    when /x86_64/
+      install_ruby
+      install_homebrew
       unless system('command -v clojure >/dev/null 2>&1')
         puts "Installing Clojure..."
-        output, status = execute_command('choco install clojure')
+        output, status = execute_command('brew install clojure')
+        puts output unless status.zero?
+      else
+        puts "Clojure is already installed."
+      end
+    when /arm64/
+      install_ruby
+      install_homebrew
+      unless system('command -v clojure >/dev/null 2>&1')
+        puts "Installing Clojure..."
+        output, status = execute_command('brew install clojure')
         puts output unless status.zero?
       else
         puts "Clojure is already installed."
       end
     else
-      puts "Unsupported OS: #{RbConfig::CONFIG['host_os']}. Cannot install Clojure."
+      puts "Unsupported architecture: #{RbConfig::CONFIG['host_cpu']}. Cannot install Clojure."
       exit(1)
     end
+  when /linux-gnu/
+    install_ruby
+    if system('command -v apt-get >/dev/null 2>&1')
+      unless system('command -v clojure >/dev/null 2>&1')
+        puts "Updating package list..."
+        execute_command('sudo apt-get update')
+
+        puts "Installing Clojure..."
+        output, status = execute_command('sudo apt-get install -y clojure')
+        puts output unless status.zero?
+      else
+        puts "Clojure is already installed."
+      end
+    elsif system('command -v dnf >/dev/null 2>&1')
+      unless system('command -v clojure >/dev/null 2>&1')
+        puts "Installing Clojure..."
+        output, status = execute_command('sudo dnf install -y clojure')
+        puts output unless status.zero?
+      else
+        puts "Clojure is already installed."
+      end
+    else
+      puts "Unsupported Linux distribution. Cannot install Clojure."
+      exit(1)
+    end
+  when /mswin|mingw|cygwin/
+    install_ruby
+    unless system('command -v clojure >/dev/null 2>&1')
+      puts "Installing Clojure..."
+      output, status = execute_command('choco install clojure')
+      puts output unless status.zero?
+    else
+      puts "Clojure is already installed."
+    end
+  else
+    puts "Unsupported OS: #{RbConfig::CONFIG['host_os']}. Cannot install Clojure."
+    exit(1)
   end
-  
-  def install_leiningen
-    case RbConfig::CONFIG['host_os']
-    when /darwin/
-      case RbConfig::CONFIG['host_cpu']
-      when /x86_64/
-        install_homebrew
-        unless system('command -v lein >/dev/null 2>&1')
-          puts "Installing Leiningen..."
-          output, status = execute_command('brew install leiningen')
-          puts output unless status.zero?
-        else
-          puts "Leiningen is already installed."
-        end
-      when /arm64/
-        install_homebrew
-        unless system('command -v lein >/dev/null 2>&1')
-          puts "Installing Leiningen..."
-          output, status = execute_command('brew install leiningen')
-          puts output unless status.zero?
-        else
-          puts "Leiningen is already installed."
-        end
-      else
-        puts "Unsupported architecture: #{RbConfig::CONFIG['host_cpu']}. Cannot install Leiningen."
-        exit(1)
-      end
-    when /linux-gnu/
-      if system('command -v apt-get >/dev/null 2>&1')
-        unless system('command -v lein >/dev/null 2>&1')
-          puts "Updating package list..."
-          execute_command('sudo apt-get update')
-  
-          puts "Installing Leiningen..."
-          output, status = execute_command('sudo apt-get install -y leiningen')
-          puts output unless status.zero?
-        else
-          puts "Leiningen is already installed."
-        end
-      elsif system('command -v dnf >/dev/null 2>&1')
-        unless system('command -v lein >/dev/null 2>&1')
-          puts "Installing Leiningen..."
-          output, status = execute_command('sudo dnf install -y leiningen')
-          puts output unless status.zero?
-        else
-          puts "Leiningen is already installed."
-        end
-      else
-        puts "Unsupported Linux distribution. Cannot install Leiningen."
-        exit(1)
-      end
-    when /mswin|mingw|cygwin/
+end
+
+def install_leiningen
+  case RbConfig::CONFIG['host_os']
+  when /darwin/
+    case RbConfig::CONFIG['host_cpu']
+    when /x86_64/
+      install_ruby
+      install_homebrew
       unless system('command -v lein >/dev/null 2>&1')
         puts "Installing Leiningen..."
-        output, status = execute_command('choco install leiningen')
+        output, status = execute_command('brew install leiningen')
+        puts output unless status.zero?
+      else
+        puts "Leiningen is already installed."
+      end
+    when /arm64/
+      install_ruby
+      install_homebrew
+      unless system('command -v lein >/dev/null 2>&1')
+        puts "Installing Leiningen..."
+        output, status = execute_command('brew install leiningen')
         puts output unless status.zero?
       else
         puts "Leiningen is already installed."
       end
     else
-      puts "Unsupported OS: #{RbConfig::CONFIG['host_os']}. Cannot install Leiningen."
+      puts "Unsupported architecture: #{RbConfig::CONFIG['host_cpu']}. Cannot install Leiningen."
       exit(1)
     end
+  when /linux-gnu/
+    install_ruby
+    if system('command -v apt-get >/dev/null 2>&1')
+      unless system('command -v lein >/dev/null 2>&1')
+        puts "Updating package list..."
+        execute_command('sudo apt-get update')
+
+        puts "Installing Leiningen..."
+        output, status = execute_command('sudo apt-get install -y leiningen')
+        puts output unless status.zero?
+      else
+        puts "Leiningen is already installed."
+      end
+    elsif system('command -v dnf >/dev/null 2>&1')
+      unless system('command -v lein >/dev/null 2>&1')
+        puts "Installing Leiningen..."
+        output, status = execute_command('sudo dnf install -y leiningen')
+        puts output unless status.zero?
+      else
+        puts "Leiningen is already installed."
+      end
+    else
+      puts "Unsupported Linux distribution. Cannot install Leiningen."
+      exit(1)
+    end
+  when /mswin|mingw|cygwin/
+    install_ruby
+    unless system('command -v lein >/dev/null 2>&1')
+      puts "Installing Leiningen..."
+      output, status = execute_command('choco install leiningen')
+      puts output unless status.zero?
+    else
+      puts "Leiningen is already installed."
+    end
+  else
+    puts "Unsupported OS: #{RbConfig::CONFIG['host_os']}. Cannot install Leiningen."
+    exit(1)
   end
-  
-  def main
-    install_clojure
-    install_leiningen
-  
-    puts "Installation complete!"
-  end
-  
-  main
-  
+end
+
+def main
+  install_clojure
+  install_leiningen
+
+  puts "Installation complete!"
+end
+
+main
